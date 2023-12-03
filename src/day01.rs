483d021c10159ff3e0bb914481376e34d8db6a7d
Sebastian Andersson <sebastian@bittr.nu>
2023-12-03 13:26:30 +0100

    Simplify day 1, part 2

--- src/day01.rs
+++ src/day01.rs
@@ -35,39 +35,23 @@ pub fn solve_part1(data: &[InputType]) -> SolutionType {
         .sum::<SolutionType>()
 }
 
-fn first_spelled_digit(hay: &str) -> Option<(char, usize, usize)> {
-    let mut found = None;
-    for (needle, c) in [
-        ("one", '1'),
-        ("two", '2'),
-        ("three", '3'),
-        ("four", '4'),
-        ("five", '5'),
-        ("six", '6'),
-        ("seven", '7'),
-        ("eight", '8'),
-        ("nine", '9'),
-    ] {
-        if let Some(pos) = hay.find(needle) {
-            if found.is_none() {
-                found = Some((c, pos, needle.len()))
-            } else if let Some((_, old_pos, _)) = found {
-                if old_pos > pos {
-                    found = Some((c, pos, needle.len()))
-                }
-            }
-        }
-    }
-    found
-}
-
 #[aoc(day1, part2)]
 pub fn solve_part2(data: &[InputType]) -> SolutionType {
     data.iter()
         .map(|line| {
             let mut line = line.to_string();
-            while let Some(found) = first_spelled_digit(&line) {
-                line.replace_range(found.1..found.1 + found.2, &found.0.to_string())
+            for (from, to) in [
+                ("one", "o1e"),
+                ("two", "t2o"),
+                ("three", "t3e"),
+                ("four", "f4r"),
+                ("five", "f5e"),
+                ("six", "s6x"),
+                ("seven", "s7n"),
+                ("eight", "e8t"),
+                ("nine", "n9e"),
+            ] {
+                line = line.replace(from, to);
             }
             let first_digit = line
                 .chars()
