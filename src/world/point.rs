c64b38ed7103d5ce5ecbcac259eef8ffaa73ab43
Sebastian Andersson <sebastian.andersson@lumenradio.com>
2023-12-11 11:46:25 +0100

    Use num crate for Point, add manhattan distance

--- src/world/point.rs
+++ src/world/point.rs
@@ -5,6 +5,7 @@
 type Length = i32;
 
 use super::dir::Dir;
+use num::*;
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 pub struct Point<T = Length> {
@@ -12,7 +13,7 @@ pub struct Point<T = Length> {
     pub y: T,
 }
 
-impl Point {
+impl<T: Signed + Copy> Point<T> {
     pub fn walk(self, dir: Dir) -> Self {
         match dir {
             Dir::None => Self {
@@ -21,37 +22,41 @@ impl Point {
             },
             Dir::North => Self {
                 x: self.x,
-                y: self.y - 1,
+                y: self.y - One::one(),
             },
             Dir::South => Self {
                 x: self.x,
-                y: self.y + 1,
+                y: self.y + One::one(),
             },
             Dir::East => Self {
-                x: self.x + 1,
+                x: self.x + One::one(),
                 y: self.y,
             },
             Dir::West => Self {
-                x: self.x - 1,
+                x: self.x - One::one(),
                 y: self.y,
             },
             Dir::NorthEast => Self {
-                x: self.x + 1,
-                y: self.y - 1,
+                x: self.x + One::one(),
+                y: self.y - One::one(),
             },
             Dir::NorthWest => Self {
-                x: self.x - 1,
-                y: self.y - 1,
+                x: self.x - One::one(),
+                y: self.y - One::one(),
             },
             Dir::SouthEast => Self {
-                x: self.x + 1,
-                y: self.y + 1,
+                x: self.x + One::one(),
+                y: self.y + One::one(),
             },
             Dir::SouthWest => Self {
-                x: self.x - 1,
-                y: self.y + 1,
+                x: self.x - One::one(),
+                y: self.y + One::one(),
             },
         }
+    }
+
+    pub fn manhattan_distance(&self, other: Self) -> T {
+        T::abs(&(self.x - other.x)) + T::abs(&(self.y - other.y))
     }
 }
 
