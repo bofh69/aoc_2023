47f487948f956fe0d34b83cac8aa0fc7ff8da0ce
Sebastian Andersson <sebastian@bittr.nu>
2023-12-12 22:04:04 +0100

    Cleaned up day12, part 1

--- src/day12.rs
+++ src/day12.rs
@@ -28,86 +28,6 @@ pub fn input_generator(input: &str) -> Vec<InputType> {
         .collect()
 }
 
-fn is_valid(springs: &Vec<u8>, groups: &Vec<u8>) -> bool {
-    let mut group_index = 0;
-    let mut is_spring_group = springs[0] == b'#';
-    let mut group_len = 0;
-    for c in springs {
-        // println!("Testing {}", *c as char);
-        if *c == b'#' {
-            if is_spring_group {
-                group_len += 1;
-                if group_len > groups[group_index] {
-                    // println!("{:?} has too large ({}) group, should be {}", springs, group_len, groups[group_index]);
-                    return false;
-                }
-            } else {
-                if group_index >= groups.len() {
-                    // println!("{:?} has too many ({}) groups, should be {}", springs, group_index, groups.len());
-                    return false;
-                }
-                is_spring_group = true;
-                group_len = 1;
-            }
-        } else if is_spring_group {
-            is_spring_group = false;
-            if group_len != groups[group_index] {
-                // println!("{:?} has incorrect ({}) group size, should be {}", springs, group_len, groups[group_index]);
-                return false;
-            }
-            group_len = 0;
-            group_index += 1;
-        }
-    }
-    if is_spring_group {
-        group_len == groups[group_index] && group_index == groups.len() - 1
-    } else {
-        group_index == groups.len()
-    }
-}
-
-#[aoc(day12, part1)]
-pub fn solve_part1(data: &[InputType]) -> SolutionType {
-    let mut scratch = vec![];
-    data.iter()
-        .map(|(springs, groups)| {
-            let mut arrangements = 0;
-            let unknowns = springs.iter().filter(|c| **c == b'?').count();
-            if unknowns == 0 {
-                if is_valid(springs, groups) {
-                    arrangements = 1;
-                }
-            } else {
-                for num in 0..(1 << unknowns) {
-                    let mut num = num;
-                    scratch.clear();
-                    for c in springs {
-                        scratch.push(if *c == b'?' {
-                            let c = if num & 1 == 1 { b'#' } else { b'.' };
-                            num >>= 1;
-                            c
-                        } else {
-                            *c
-                        });
-                    }
-                    if is_valid(&scratch, groups) {
-                        /*
-                        print!("VALID: ");
-                        for c in &scratch {
-                            print!("{}", *c as char);
-                        }
-                        println!();
-                        */
-                        arrangements += 1;
-                    }
-                }
-            }
-            // println!("{:?} - {} arrangements", springs, arrangements);
-            arrangements
-        })
-        .sum()
-}
-
 type HashKey<'a> = (bool, u8, &'a [u8], u8, &'a [u8]);
 
 fn count_arrangements<'a>(
@@ -233,26 +153,29 @@ fn count_arrangements_<'a>(
     }
 }
 
+#[aoc(day12, part1)]
+pub fn solve_part1(data: &[InputType]) -> SolutionType {
+    data.par_iter()
+        .map(|(springs, groups)| {
+            let mut cache = HashMap::new();
+            count_arrangements(&mut cache, false, springs[0], &springs[1..], 0, groups)
+        })
+        .sum()
+}
+
 #[aoc(day12, part2)]
 pub fn solve_part2(data: &[InputType]) -> SolutionType {
     data.par_iter()
         .map(|(spring, group)| {
             let mut spring2 = vec![];
-            spring2.extend_from_slice(spring);
-            spring2.push(b'?');
-            spring2.extend_from_slice(spring);
-            spring2.push(b'?');
-            spring2.extend_from_slice(spring);
-            spring2.push(b'?');
-            spring2.extend_from_slice(spring);
-            spring2.push(b'?');
-            spring2.extend_from_slice(spring);
             let mut groups2 = vec![];
+            spring2.extend_from_slice(spring);
             groups2.extend_from_slice(group);
-            groups2.extend_from_slice(group);
-            groups2.extend_from_slice(group);
-            groups2.extend_from_slice(group);
-            groups2.extend_from_slice(group);
+            for _ in 0..4 {
+                spring2.push(b'?');
+                spring2.extend_from_slice(spring);
+                groups2.extend_from_slice(group);
+            }
             (spring2, groups2)
         })
         .map(|(springs, groups)| {
