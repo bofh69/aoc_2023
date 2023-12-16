44c42a15d1031e935161ad88ee08bac7f5efa90c
Sebastian Andersson <sebastian@bittr.nu>
2023-12-16 20:04:20 +0100

    Optimized day 5, part 2

--- src/day05.rs
+++ src/day05.rs
@@ -3,6 +3,9 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 use aoc_runner_derive::{aoc, aoc_generator};
+// use rayon::prelude::*;
+use std::collections::HashSet;
+use std::ops::Range;
 
 type Seed = i64;
 
@@ -100,19 +103,188 @@ pub fn solve_part1(data: &InputType) -> SolutionType {
         .expect("Minimum number")
 }
 
+fn translate_range_with_mapping(
+    translated: &mut Vec<Range<Seed>>,
+    untranslated: &mut HashSet<Range<Seed>>,
+    range: Range<Seed>,
+    mapping: &(Seed, Seed, Seed),
+) {
+    if range.end <= mapping.1 || range.start >= mapping.1 + mapping.2 {
+        // Outside range
+        untranslated.insert(range);
+    } else if range.start < mapping.1 {
+        // Starts before
+        untranslated.insert(range.start..mapping.1);
+        if range.end <= mapping.1 + mapping.2 {
+            // Ends inside
+            translated.push(mapping.0..(mapping.0 + (range.end - mapping.1)));
+        } else {
+            // Ends after
+            translated.push(mapping.0..(mapping.0 + mapping.2));
+            untranslated.insert((mapping.1 + mapping.2)..range.end);
+        }
+    } else {
+        // Starts inside
+        if range.end <= mapping.1 + mapping.2 {
+            // Ends inside
+            translated
+                .push((mapping.0 + range.start - mapping.1)..(mapping.0 + (range.end - mapping.1)));
+        } else {
+            // Ends after
+            translated.push((mapping.0 + range.start - mapping.1)..(mapping.0 + mapping.2));
+            untranslated.insert((mapping.1 + mapping.2)..range.end);
+        }
+    }
+}
+
+fn translate_range_with_mappings(
+    range: Range<Seed>,
+    mappings: &Vec<(Seed, Seed, Seed)>,
+) -> Vec<Range<Seed>> {
+    let mut untranslated = HashSet::from([range]);
+    let mut translated = vec![];
+    for mapping in mappings {
+        let mut next_untranslated = HashSet::new();
+        for range in untranslated {
+            translate_range_with_mapping(
+                &mut translated,
+                &mut next_untranslated,
+                range.clone(),
+                mapping,
+            );
+        }
+        untranslated = next_untranslated;
+    }
+    // Put untranslated in translated
+    for range in untranslated {
+        translated.push(range);
+    }
+    translated
+}
+
+fn translate_range(
+    range: Range<Seed>,
+    mappings: &[MappingData],
+    kind: &str,
+    final_kind: &str,
+) -> Vec<Range<Seed>> {
+    let mut kind = kind;
+    let mut ranges = vec![range];
+    while kind != final_kind {
+        for trans in mappings.iter() {
+            if trans.from_kind == kind {
+                kind = trans.to_kind.as_str();
+                let mut next_ranges = HashSet::new();
+                for range in ranges {
+                    for new_range in translate_range_with_mappings(range, &trans.ranges).iter() {
+                        next_ranges.insert(new_range.clone());
+                    }
+                }
+                ranges = vec![];
+                for range in next_ranges {
+                    ranges.push(range);
+                }
+            }
+        }
+    }
+    ranges
+}
+
 #[aoc(day5, part2)]
 pub fn solve_part2(data: &InputType) -> SolutionType {
+    // Should really translate ranges
     let mut numbers = data.0.iter();
     let mut location = SolutionType::MAX;
     while let Some(start) = numbers.next() {
         let size = numbers.next().expect("seed size");
-        for i in *start..(start + size) {
-            let min = translate_number(i, "seed", "location", data);
-            if min < location {
-                println!("New min {}", min);
-                location = min;
+        let range = *start..(start + size);
+        let new_range = translate_range(range, &data.1, "seed", "location");
+        for range in new_range {
+            if range.start < location {
+                location = range.start;
             }
         }
     }
     location
+}
+
+#[cfg(test)]
+mod test {
+    use super::translate_range_with_mapping;
+    use super::translate_range_with_mappings;
+    use std::collections::HashSet;
+
+    #[test]
+    fn translate_range_before() {
+        let mut untranslated = HashSet::new();
+        let mut translated = vec![];
+        let range = 30..40;
+        let mapping = (20, 40, 10);
+        translate_range_with_mapping(&mut translated, &mut untranslated, range.clone(), &mapping);
+        assert!(translated.is_empty());
+        assert_eq!(untranslated, HashSet::from([range]));
+    }
+
+    #[test]
+    fn translate_range_after() {
+        let mut untranslated = HashSet::new();
+        let mut translated = vec![];
+        let range = 50..60;
+        let mapping = (20, 40, 10);
+        translate_range_with_mapping(&mut translated, &mut untranslated, range.clone(), &mapping);
+        assert!(translated.is_empty());
+        assert_eq!(untranslated, HashSet::from([range]));
+    }
+
+    #[test]
+    fn translate_range_starts_before_ends_inside() {
+        let mut untranslated = HashSet::new();
+        let mut translated = vec![];
+        let range = 35..45;
+        let mapping = (20, 40, 10);
+        translate_range_with_mapping(&mut translated, &mut untranslated, range.clone(), &mapping);
+        assert_eq!(untranslated, HashSet::from([35..40]));
+        assert_eq!(translated, vec![20..25]);
+    }
+
+    #[test]
+    fn translate_range_starts_before_ends_after() {
+        let mut untranslated = HashSet::new();
+        let mut translated = vec![];
+        let range = 35..55;
+        let mapping = (20, 40, 10);
+        translate_range_with_mapping(&mut translated, &mut untranslated, range.clone(), &mapping);
+        assert_eq!(untranslated, HashSet::from([35..40, 50..55]));
+        assert_eq!(translated, vec![20..30]);
+    }
+
+    #[test]
+    fn translate_range_starts_inside_ends_after() {
+        let mut untranslated = HashSet::new();
+        let mut translated = vec![];
+        let range = 45..55;
+        let mapping = (20, 40, 10);
+        translate_range_with_mapping(&mut translated, &mut untranslated, range.clone(), &mapping);
+        assert_eq!(untranslated, HashSet::from([50..55]));
+        assert_eq!(translated, vec![25..30]);
+    }
+
+    #[test]
+    fn translate_range_is_inside() {
+        let mut untranslated = HashSet::new();
+        let mut translated = vec![];
+        let range = 42..48;
+        let mapping = (20, 40, 10);
+        translate_range_with_mapping(&mut translated, &mut untranslated, range.clone(), &mapping);
+        assert!(untranslated.is_empty());
+        assert_eq!(translated, vec![22..28]);
+    }
+
+    #[test]
+    fn translate_range_with_mappings_inside() {
+        let range = 82..83;
+        let mappings = vec![(50, 98, 2), (52, 50, 48)];
+        let result = translate_range_with_mappings(range, &mappings);
+        assert_eq!(result, vec![84..85]);
+    }
 }
