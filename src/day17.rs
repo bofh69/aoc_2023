d5e3b4d3853d6e5c7c957f89451f03d831c9950b
Sebastian Andersson <sebastian@bittr.nu>
2023-12-17 11:17:23 +0100

    Use A* for day17 part 2

--- src/day17.rs
+++ src/day17.rs
@@ -9,7 +9,7 @@ use super::world::*;
 // use std::collections::HashMap;
 // use rayon::prelude::*;
 
-type SolutionType = usize;
+type SolutionType = u16;
 
 #[aoc_generator(day17)]
 pub fn input_generator(input: &str) -> Map {
@@ -17,15 +17,15 @@ pub fn input_generator(input: &str) -> Map {
 }
 
 #[derive(Debug, Clone, Eq, PartialEq)]
-struct CostAndPoint(SolutionType, u8, Point, Dir);
+struct CostAndPoint<T: Eq + PartialEq>(SolutionType, T);
 
-impl Ord for CostAndPoint {
+impl<T: Eq + PartialEq> Ord for CostAndPoint<T> {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         other.0.cmp(&self.0)
     }
 }
 
-impl PartialOrd for CostAndPoint {
+impl<T: Eq + PartialEq> PartialOrd for CostAndPoint<T> {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some(other.0.cmp(&self.0))
     }
@@ -34,9 +34,9 @@ impl PartialOrd for CostAndPoint {
 fn bfs(map: &Map, from: Point, to: Point) -> SolutionType {
     let mut expanded = std::collections::HashMap::new();
     let mut to_expand = std::collections::BinaryHeap::new();
-    to_expand.push(CostAndPoint(0, 1, from.walk(Dir::East), Dir::East));
-    to_expand.push(CostAndPoint(0, 1, from.walk(Dir::South), Dir::South));
-    while let Some(CostAndPoint(mut heat_loss, steps, pos, dir)) = to_expand.pop() {
+    to_expand.push(CostAndPoint(0, (1, from.walk(Dir::East), Dir::East)));
+    to_expand.push(CostAndPoint(0, (1, from.walk(Dir::South), Dir::South)));
+    while let Some(CostAndPoint(mut heat_loss, (steps, pos, dir))) = to_expand.pop() {
         if let Some(cost) = expanded.get_mut(&(steps, pos, dir)) {
             if *cost <= heat_loss {
                 continue;
@@ -47,22 +47,23 @@ fn bfs(map: &Map, from: Point, to: Point) -> SolutionType {
         }
         heat_loss += SolutionType::from(map.get_at_unchecked(pos) - b'0');
         if to == pos {
+            // println!("{} ", expanded.len());
             return heat_loss;
         }
         let dir_left = dir.turn_cardinal_left();
         let pos_left = pos.walk(dir_left);
         if map.is_inside_map(pos_left) {
-            to_expand.push(CostAndPoint(heat_loss, 1, pos_left, dir_left));
+            to_expand.push(CostAndPoint(heat_loss, (1, pos_left, dir_left)));
         }
         let dir_right = dir.turn_cardinal_right();
         let pos_right = pos.walk(dir_right);
         if map.is_inside_map(pos_right) {
-            to_expand.push(CostAndPoint(heat_loss, 1, pos_right, dir_right));
+            to_expand.push(CostAndPoint(heat_loss, (1, pos_right, dir_right)));
         }
         if steps < 3 {
             let pos = pos.walk(dir);
             if map.is_inside_map(pos) {
-                to_expand.push(CostAndPoint(heat_loss, steps + 1, pos, dir));
+                to_expand.push(CostAndPoint(heat_loss, (steps + 1, pos, dir)));
             }
         }
     }
@@ -81,9 +82,11 @@ pub fn solve_part1(map: &Map) -> SolutionType {
 fn bfs2(map: &Map, from: Point, to: Point) -> SolutionType {
     let mut expanded = std::collections::HashMap::new();
     let mut to_expand = std::collections::BinaryHeap::new();
-    to_expand.push(CostAndPoint(0, 1, from.walk(Dir::East), Dir::East));
-    to_expand.push(CostAndPoint(0, 1, from.walk(Dir::South), Dir::South));
-    while let Some(CostAndPoint(mut heat_loss, steps, pos, dir)) = to_expand.pop() {
+    to_expand.push(CostAndPoint(0, (0, 1, from.walk(Dir::East), Dir::East)));
+    to_expand.push(CostAndPoint(0, (0, 1, from.walk(Dir::South), Dir::South)));
+    while let Some(CostAndPoint(_estimated_cost, (mut heat_loss, steps, pos, dir))) =
+        to_expand.pop()
+    {
         if let Some(cost) = expanded.get_mut(&(steps, pos, dir)) {
             if *cost <= heat_loss {
                 continue;
@@ -93,25 +96,36 @@ fn bfs2(map: &Map, from: Point, to: Point) -> SolutionType {
             expanded.insert((steps, pos, dir), heat_loss);
         }
         heat_loss += SolutionType::from(map.get_at_unchecked(pos) - b'0');
+        let estimated_cost = (heat_loss as i32 + pos.manhattan_distance(to)) as u16;
         if to == pos {
+            // println!("{} ", expanded.len());
             return heat_loss;
         }
         if steps >= 4 {
             let dir_left = dir.turn_cardinal_left();
             let pos_left = pos.walk(dir_left);
             if map.is_inside_map(pos_left) {
-                to_expand.push(CostAndPoint(heat_loss, 1, pos_left, dir_left));
+                to_expand.push(CostAndPoint(
+                    estimated_cost,
+                    (heat_loss, 1, pos_left, dir_left),
+                ));
             }
             let dir_right = dir.turn_cardinal_right();
             let pos_right = pos.walk(dir_right);
             if map.is_inside_map(pos_right) {
-                to_expand.push(CostAndPoint(heat_loss, 1, pos_right, dir_right));
+                to_expand.push(CostAndPoint(
+                    estimated_cost,
+                    (heat_loss, 1, pos_right, dir_right),
+                ));
             }
         }
         if steps < 10 {
             let pos = pos.walk(dir);
             if map.is_inside_map(pos) {
-                to_expand.push(CostAndPoint(heat_loss, steps + 1, pos, dir));
+                to_expand.push(CostAndPoint(
+                    estimated_cost,
+                    (heat_loss, steps + 1, pos, dir),
+                ));
             }
         }
     }
